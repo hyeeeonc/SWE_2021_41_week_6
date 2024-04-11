3b2bb18330ff1f77df785a6523c4750e178d8432
hyeeeonc <hyeeeonc@gmail.com>
2024-04-11 18:46:03 +0900

    fix main.py: change exception case

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ def twoSum(nums: List[int], target: int) -> List[int]:
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
-    return [nums[0], nums[1]]
+    return [0, 1]
